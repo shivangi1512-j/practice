bed4f4eabbbb7c23cd75215cf07c38cd14114433
shivangi1512-j <shivangiba1512@gmail.com>
2025-07-08 12:16:44 +0530

    Add files via upload

---
+++ index8.py
@@ -0,0 +1,25 @@
+print("welcome to theInteractive personal data collector!")
+
+name=(input("please enter your name:"))
+age=(input("please enter your age:"))
+height=(input("please enter your height in meters:"))
+num=(input("please enter your favourite number:"))
+
+print("thank you! here is the information we collectes!")
+
+name=str("alice");
+age=int("25");
+height=float("1.68");
+favouritenumber=int("7");
+
+print("name:alice", type(name))
+
+print("age:25", type(age))
+
+print("height:1.68", type(height))
+
+print("favourite number:7", type(favouritenumber))
+
+print("your birth year is approximately: 1889(based on your age of 25)")
+
+print("thank you for using the personal data collector,Goodbye ! ")
